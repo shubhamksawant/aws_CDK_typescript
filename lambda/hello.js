f77cf00ceedfb12e56a2c240de409b75f80396c6
EC2 Default User <ec2-user@ip-172-31-41-72.ap-south-1.compute.internal>
2023-02-04 06:54:50 +0000

    added files for lamda function and api gateway

---
+++ lambda/hello.js
@@ -0,0 +1,12 @@
+exports.handler = async function(event ){
+    
+    console.log("request:", JSON.stringify(event,undefined,2));
+    return{
+        
+        statusCode: 200,
+        headers: {"Content-Type": "text/plain" },
+        body: 'Hello , CDK! You have hit ${event.path}\n '
+        };
+    };
+
+

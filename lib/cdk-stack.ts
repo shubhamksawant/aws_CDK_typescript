bd6bd7f34e5a5f8f628be9922dc845b0b6c21cb5
dev1 <jwowshubham@gmail.com>
2023-02-04 12:57:23 +0530

    added files for hit counter application

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 
 export class CdkStack extends cdk.Stack {
@@ -13,9 +14,14 @@ export class CdkStack extends cdk.Stack {
       handler: 'hello.handler'                // file is "hello", function is "handler"
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+
 // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+       handler: helloWithCounter.handler
     });
 
 

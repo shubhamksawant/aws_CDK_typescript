ec4d839402c398dcc1d36c1b3d774381135a703a
dev1 <jwowshubham@gmail.com>
2023-02-04 13:05:20 +0530

    hit conter with table view

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { HitCounter } from './hitcounter';
-
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -24,6 +24,10 @@ export class CdkStack extends cdk.Stack {
        handler: helloWithCounter.handler
     });
 
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    });
 
   }
 }
--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -9,33 +9,37 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
-
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
+
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    const table = new dynamodb.Table(this, 'Hits', {
-        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    const table = new dynamodb.Table(this, "Hits", {
+      partitionKey: {
+        name: "path",
+        type: dynamodb.AttributeType.STRING
+      }
     });
+    this.table = table;
 
     this.handler = new lambda.Function(this, 'HitCounterHandler', {
-        runtime: lambda.Runtime.NODEJS_14_X,
-        handler: 'hitcounter.handler',
-        code: lambda.Code.fromAsset('lambda'),
-        environment: {
-            DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-            HITS_TABLE_NAME: table.tableName
-        }
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hitcounter.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
     });
 
+    // grant the lambda role read/write permissions to our table
+    table.grantReadWriteData(this.handler);
 
-     // grant the lambda role read/write permissions to our table
-     table.grantReadWriteData(this.handler);
-
-      // grant the lambda role invoke permissions to the downstream function
-     props.downstream.grantInvoke(this.handler);
-
+    // grant the lambda role invoke permissions to the downstream function
+    props.downstream.grantInvoke(this.handler);
   }
 }
